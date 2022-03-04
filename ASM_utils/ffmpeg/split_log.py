34d206f8ea79a89c45a23d6faee230aeb7f9d0a1
Kirtan Shah <kishah@ucsd.edu>
2022-03-04 00:34:42 -0800

    Add split log script

---
+++ ASM_utils/ffmpeg/split_log.py
@@ -0,0 +1,29 @@
+# usage: split_log [output_stats_file] [output_info_file]
+
+import sys
+import os
+import pathlib
+import argparse
+
+parser = argparse.ArgumentParser(description='Split ffmpeg log files.')
+parser.add_argument('output_stats_file', help='file path for stats file')
+parser.add_argument('output_info_file', help='file path for info file')
+args = parser.parse_args()
+
+pathlib.Path(os.path.dirname(args.output_stats_file)).mkdir(parents=True, exist_ok=True)
+pathlib.Path(os.path.dirname(args.output_info_file)).mkdir(parents=True, exist_ok=True)
+
+stats_file = open(args.output_stats_file, 'ab+')
+info_file = open(args.output_info_file, 'ab+')
+
+line = bytearray()
+while True:
+  b_in = sys.stdin.buffer.read1(1)
+  line.extend(b_in)
+  if b_in == b'\n' or b_in == b'\r':
+    out_file = stats_file if line.decode().startswith('frame') else info_file
+    out_file.write(line)
+    line = bytearray()
+
+stats_file.close()
+info_file.close()
