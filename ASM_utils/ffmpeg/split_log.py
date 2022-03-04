0d98306c3688c09d18aee70d3b5ce9890abcdf09
Kirtan Shah <kishah@ucsd.edu>
2022-03-04 00:42:30 -0800

    Add main guard

--- ASM_utils/ffmpeg/split_log.py
+++ ASM_utils/ffmpeg/split_log.py
@@ -5,25 +5,29 @@ import os
 import pathlib
 import argparse
 
-parser = argparse.ArgumentParser(description='Split ffmpeg log files.')
-parser.add_argument('output_stats_file', help='file path for stats file')
-parser.add_argument('output_info_file', help='file path for info file')
-args = parser.parse_args()
+def process_output():
+    parser = argparse.ArgumentParser(description='Split ffmpeg log files.')
+    parser.add_argument('output_stats_file', help='file path for stats file')
+    parser.add_argument('output_info_file', help='file path for info file')
+    args = parser.parse_args()
 
-pathlib.Path(os.path.dirname(args.output_stats_file)).mkdir(parents=True, exist_ok=True)
-pathlib.Path(os.path.dirname(args.output_info_file)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.dirname(args.output_stats_file)).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(os.path.dirname(args.output_info_file)).mkdir(parents=True, exist_ok=True)
 
-stats_file = open(args.output_stats_file, 'ab+')
-info_file = open(args.output_info_file, 'ab+')
+    stats_file = open(args.output_stats_file, 'ab+')
+    info_file = open(args.output_info_file, 'ab+')
 
-line = bytearray()
-while True:
-  b_in = sys.stdin.buffer.read1(1)
-  line.extend(b_in)
-  if b_in == b'\n' or b_in == b'\r':
-    out_file = stats_file if line.decode().startswith('frame') else info_file
-    out_file.write(line)
     line = bytearray()
+    while True:
+        b_in = sys.stdin.buffer.read1(1)
+        line.extend(b_in)
+        if b_in == b'\n' or b_in == b'\r':
+            out_file = stats_file if line.decode().startswith('frame') else info_file
+            out_file.write(line)
+            line = bytearray()
 
-stats_file.close()
-info_file.close()
+    stats_file.close()
+    info_file.close()
+
+if __name__ == "__main__":
+    process_output()
